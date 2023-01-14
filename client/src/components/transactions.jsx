78113f256c96d8d3289909e0ad04b2ba663f9362
Marko Mandic <mandicm223@gmail.com>
2023-01-14 15:29:26 +0100

    Gifs fetched and footer done

--- client/src/components/transactions.jsx
+++ client/src/components/transactions.jsx
@@ -6,9 +6,12 @@ import { TransactionContext } from "../context/TransactionContext";
 
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
+import useFetch from "../hooks/useFetch";
 
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+    const gifUrl = useFetch({keyword})
+    
     return (
     <div className="bg-[#181918] m-4 flex flex-1
         2xl:min-w-[450px]
@@ -41,13 +44,15 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                     </>
                 )}
 
+            </div>
+
+            <img src={gifUrl || url } alt="gif" className="w-full h-64 2x:h-96 rounded-medium shadow-lg object-cover" />
+
                 <div className="bg-black p-3 px-5 w-max -mt-5 shadow-2xl">
                     <p className="text-[#37c7da] font-bold">
                         {timestamp}
                     </p>
                 </div>
-
-            </div>
         </div>
     </div>
 )}
